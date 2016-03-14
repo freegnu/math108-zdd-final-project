f247ef6dc9de3271753f78c625366ad7932fb6bd
travis14 <travis14@stanford.edu>
2016-03-13 20:00:54 -0700

    final commit

--- ZDD.py
+++ ZDD.py
@@ -2,6 +2,9 @@ import random
 import time
 import matplotlib.pyplot as plt
 import sys
+import utils
+from utils import total_size
+import numpy
 
 sys.setrecursionlimit(100000)
 
@@ -58,7 +61,7 @@ def Union(A, B):
 		LOestNode = A
 		while LOestNode.LO != None and LOestNode.LO.root != 'TRUE':
 			LOestNode = LOestNode.LO
-		if LOestNode.LO.root != 'TRUE':
+		if not LOestNode.LO or LOestNode.LO.root != 'TRUE':
 			LOestNode.LO = ZDD('TRUE', None, None)
 		return A
 	elif A.root == 'TRUE' and not B.root == 'FALSE' and not B.root == 'TRUE':
@@ -81,18 +84,43 @@ def Union(A, B):
 			C = Union(B.LO, A)
 			return ZDD(w, C, B.HI)
 
+def countSets(A):
+	if A.root == 'FALSE':
+		return 0
+	elif A.root == 'TRUE':
+		return 1
+	return countSets(A.LO) + countSets(A.HI)
+
+def testDictionary():
+	allWords = set()
+	for line in open('sgb-words.txt'):
+		wordSet = set()
+		word = str(line.strip())
+		for index, c in enumerate(word):
+			wordSet.add(index * 26 + ord(c)%32)
+		wordSet = frozenset(wordSet)
+		allWords.add(wordSet)
+	zddDictionary = constructZDD(allWords)
+	return zddDictionary
+
 def test():
 	#basic construction and representation 
 	zdd1 = constructZDD(set([frozenset([2])]))
 	print getZDDFamily(zdd1)
+	print countSets(zdd1)
 	zdd2 = constructZDD(set([frozenset([2, 3, 4, 5]), frozenset([1, 6, 7]), frozenset([1, 2, 3, 4, 5, 6])]))
 	print getZDDFamily(zdd2)
-
+	print countSets(zdd2)
+	zdd3 = constructZDD(set([frozenset([1, 3, 4, 5]), frozenset([1, 6, 7]), frozenset([1, 2, 3, 4, 5, 6]), frozenset([1, 2, 3, 6]), frozenset([1, 2, 5, ]), frozenset([1, 9, 10]), frozenset([69])]))
+	print getZDDFamily(zdd3)
+	print countSets(zdd3)
 	#code demonstrating the Union function
+	'''
 	zdd1 = constructZDD(set([frozenset([1, 2]), frozenset([3]), frozenset([4])])) 
 	zdd2 = constructZDD(set([frozenset([1, 2]), frozenset([1, 3]), frozenset([4]), frozenset([4, 5])]))
 	zdd3 = Union(zdd1, zdd2)
 	print getZDDFamily(zdd3)
+	'''
 
 	#time complexity testing code
 	'''
@@ -143,6 +171,50 @@ def test():
 	plt.savefig("zdd_n_complexity.png")
 	plt.plot(x, y)
 	'''
+	'''
+	maxBElement = 2000
+	x = []
+	y = []
+	x2 = []
+	y2 = []
+	for i in range(1, maxBElement, 5):
+		A = constructZDD(set([frozenset([i + maxBElement for i in range(i)])]))
+		B = constructZDD(set([frozenset([i for i in range(i)])]))
+		time1 = time.time()
+		C = Union(A, B)
+		runningTime = time.time() - time1
+
+		x.append(i)
+		y.append(runningTime)
+	plt.plot(x, y, 'ro')
+	plt.xlabel("Number of Nodes in A and B")
+	plt.ylabel('Running Time')
+	plt.title('ZDD Union(A, B) Running Time vs. Number of Nodes in A and B')
+	#plt.plot(x, y)
+	#plt.plot(x, numpy.poly1d(numpy.polyfit(x, y, 1))(x))
+	plt.savefig("zdd_union_complexity.png")
+	'''
+	'''
+	plt.plot(x, y, 'ro')
+	plt.xlabel("Number of Nodes in ZDD")
+	plt.ylabel('Running Time')
+	plt.title('ZDD Node Count Running Time vs. Number of Nodes in B')
+	plt.plot(x, y)
+	#plt.plot(x, numpy.poly1d(numpy.polyfit(x, y, 1))(x))
+	plt.savefig("zdd_count_complexity.png")
+	'''
+
+	'''
+	plt.plot(x, y, 'ro')
+	plt.xlabel("Number of Nodes in B\'s ZDD")
+	plt.ylabel('Running Time')
+	plt.title('ZDD Counting Sets Running Time vs. Number of Nodes')
+	plt.plot(x, y)
+	#plt.plot(x, numpy.poly1d(numpy.polyfit(x, y, 1))(x))
+	plt.savefig("zdd_count_complexity.png")
+	'''
 
 test()
+#zddDictionary = testDictionary()
+#print getZDDFamily(zddDictionary)
 
